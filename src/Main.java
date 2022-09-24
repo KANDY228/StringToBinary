89730dbb8920bd7107c133539d8b71abf9e56096
KANDY228 <71720867+KANDY228@users.noreply.github.com>
2022-09-24 19:57:28 +0300

    ToBinaryString
    bitwise shift to the left

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x = 64;
+		int y = 3;
+		int z = (x<<y);
+		System.out.println(Integer.toBinaryString(x));
+		System.out.println(Integer.toBinaryString(z));
+
+	}
+
+}
